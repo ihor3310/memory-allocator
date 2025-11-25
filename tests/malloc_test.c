8ee513952fdae7d9a72df9ea1a08a1580d2d1086
rogi <96204825+ihor3310@users.noreply.github.com>
2025-11-25 23:32:21 +0200

    arena

--- tests/malloc_test.c
+++ tests/malloc_test.c
@@ -2,20 +2,20 @@
 #include "../headers/malloc.h"
 
 int main() {
-    int *a = malloc(5 * sizeof(int));
-    if (!a) return 1;
-
-    for (int i = 0; i < 5; i++) a[i] = i + 1;
-    for (int i = 0; i < 5; i++) printf("%d ", a[i]);
+    int *arr = malloc(10 * sizeof(int));
+    for (int i=0;i<10;i++) arr[i]=i*2;
+    for (int i=0;i<10;i++) printf("%d ", arr[i]);
     printf("\n");
 
     char *s = malloc(20);
-    if (!s) return 1;
-
-    for (int i = 0; i < 19; i++) s[i] = 'A' + (i % 26);
-    s[19] = 0;
-
+    for (int i=0;i<19;i++) s[i]='a'+i%26;
+    s[19]=0;
     printf("%s\n", s);
+
+    int *b = malloc(5*sizeof(int));
+    for (int i=0;i<5;i++) b[i]=i+100;
+    for (int i=0;i<5;i++) printf("%d ", b[i]);
+    printf("\n");
 
     return 0;
 }
