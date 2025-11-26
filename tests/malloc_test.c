63ae7bf572e6a307b1fe85fccff9302f06a5cd6c
rogi <96204825+ihor3310@users.noreply.github.com>
2025-11-26 16:32:51 +0200

    Update malloc_test.c

--- tests/malloc_test.c
+++ tests/malloc_test.c
@@ -1,21 +1,61 @@
-#include <stdio.h>
+#include <CUnit/CUnit.h>
+#include <CUnit/Basic.h>
+#include <string.h>
 #include "../include/allocator.h"
 
-int main() {
+void test_malloc_int_array(void) {
     int *arr = arena_malloc(10 * sizeof(int));
-    for (int i=0;i<10;i++) arr[i]=i*2;
-    for (int i=0;i<10;i++) printf("%d ", arr[i]);
-    printf("\n");
+    CU_ASSERT_PTR_NOT_NULL(arr);
 
+    for (int i = 0; i < 10; i++)
+        arr[i] = i * 2;
+
+    for (int i = 0; i < 10; i++)
+        CU_ASSERT_EQUAL(arr[i], i * 2);
+
+    arena_free(arr);
+}
+
+void test_malloc_char_array(void) {
     char *s = arena_malloc(20);
-    for (int i=0;i<19;i++) s[i]='a'+i%26;
-    s[19]=0;
-    printf("%s\n", s);
+    CU_ASSERT_PTR_NOT_NULL(s);
 
-    int *b = arena_malloc(5*sizeof(int));
-    for (int i=0;i<5;i++) b[i]=i+100;
-    for (int i=0;i<5;i++) printf("%d ", b[i]);
-    printf("\n");
+    for (int i = 0; i < 19; i++)
+        s[i] = 'a' + i % 26;
+    s[19] = 0;
 
-    return 0;
+    for (int i = 0; i < 19; i++)
+        CU_ASSERT_EQUAL(s[i], 'a' + i % 26);
+    CU_ASSERT_EQUAL(s[19], 0);
+
+    arena_free(s);
+}
+
+void test_malloc_small_int_array(void) {
+    int *b = arena_malloc(5 * sizeof(int));
+    CU_ASSERT_PTR_NOT_NULL(b);
+
+    for (int i = 0; i < 5; i++)
+        b[i] = i + 100;
+
+    for (int i = 0; i < 5; i++)
+        CU_ASSERT_EQUAL(b[i], i + 100);
+
+    arena_free(b);
+}
+
+int main(void) {
+    CU_initialize_registry();
+    CU_pSuite suite = CU_add_suite("arena_malloc_suite", NULL, NULL);
+
+    CU_add_test(suite, "malloc int array", test_malloc_int_array);
+    CU_add_test(suite, "malloc char array", test_malloc_char_array);
+    CU_add_test(suite, "malloc small int array", test_malloc_small_int_array);
+
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    unsigned int failures = CU_get_number_of_tests_failed();
+    CU_cleanup_registry();
+
+    return (failures > 0) ? 1 : 0;
 }
